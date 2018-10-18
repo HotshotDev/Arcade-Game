bccf8f1142248dfe7e09b8ac6805d58b47d48903
Aaron-Kelly <aaronk-7@hotmail.com>
2018-10-18 20:39:34 +0100

    Arcade

---
+++ README.md
@@ -0,0 +1,24 @@
+frontend-nanodegree-arcade-game
+===============================
+
+Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+
+For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+# Arcade Game Project (Frogger)
+
+## How to Play The Game
+
+To play the matching game, You must load it up on your browser.
+Once you load up the game you will be presented with the game.
+There is a how to play on the left hand side of the screen.
+
+ - Use your arrow key to move the player.
+ - You need to reach the water without hitting off a bug.
+ - If you get hit by a bug, you lose a life.
+ - If you get to the water safely, You gain 100 points!
+ - The higher your score, The faster they move!
+
+### Features
+
+- Scoreboard
---
+++ js/app.js
@@ -0,0 +1,194 @@
+let low = 10,
+    medium = 55,
+    high = 100;
+var ScoreBoard;
+var allEnemies = [];
+
+ScoreBoard = document.createElement('h1');
+document.body.appendChild(ScoreBoard);
+
+var GameObjects = function (x, y) {
+    this.startPoint = 0;
+    this.endPoint = 550;
+    this.x = 0;
+    this.y = 0;
+    this.sprite;
+};
+
+var Enemy = function (x, y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    GameObjects.call(this, Enemy);
+    this.sprite = 'images/enemy-bug.png';
+    this.x = (Math.floor(Math.random() * (-2000)) + 2);
+};
+Enemy.prototype = Object.create(GameObjects.prototype);
+
+//set the constructor from object to enemy
+Enemy.prototype.constructor = Enemy;
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function (dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+
+    this.y = 60;
+    for (let i = 0; i < allEnemies.length; i++) {
+        allEnemies[i].startPoint = (Math.floor(Math.random() * (-2000)) + 2);
+        //Changes the speed of the bugs depending on your score
+        if (player.score === 0 || player.score <= 2000) {
+            allEnemies[i].x += (low * dt);
+        } else if (player.score >= 1001 || player.score <= 5000) {
+            allEnemies[i].x += (medium * dt);
+        } else {
+            allEnemies[i].x += (high * dt);
+        }
+
+        //Set the y-coordinate of the enemy
+        allEnemies[i].y = allEnemies[i].y + 80;
+        //Ensure your bugs doesnt go beyond their vertical limits (y-axis)
+        if (allEnemies[i].y > 220) {
+            allEnemies[i].y = 60;
+        }
+        //Ensure your bugs doesnt go beyond their horizontal limits x-axis
+        if (allEnemies[i].x >= this.endPoint) {
+            allEnemies[i].x = allEnemies[i].startPoint;
+        }
+
+    }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+var enemy6 = new Enemy();
+var enemy1 = new Enemy();
+var enemy2 = new Enemy();
+var enemy3 = new Enemy();
+var enemy4 = new Enemy();
+var enemy5 = new Enemy();
+var enemy6 = new Enemy();
+var enemy7 = new Enemy();
+allEnemies.push(enemy1);
+allEnemies.push(enemy6);
+allEnemies.push(enemy2);
+allEnemies.push(enemy3);
+allEnemies.push(enemy4);
+allEnemies.push(enemy5);
+allEnemies.push(enemy6);
+allEnemies.push(enemy7);
+
+var Player = function () {
+
+    GameObjects.call(this, Player);
+    this.sprite = 'images/char-boy.png';
+    this.score = 0;
+    this.lives = 3;
+    this.x = 200;
+    this.y = 380;
+}
+
+Player.prototype = Object.create(GameObjects);
+Player.prototype.constructor = Player;
+
+Player.prototype.update = function () {
+    //Scoreboard update
+    ScoreBoard.innerHTML = `Score: ${player.score} Lives: ${player.lives}`;
+
+};
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    checkCollision(this.x, this.y);
+};
+
+
+Player.prototype.handleInput = function (userInput) {
+    //checks the keys pressed
+    switch (userInput) {
+        case 'left':
+            if (player.x >= 50) {
+                player.x -= 100;
+            }
+            break;
+        case 'right':
+            if (player.x <= 300) {
+                player.x += 100;
+            }
+            break;
+        case 'up':
+            if (player.y > 60) {
+                player.y -= 80;
+            } else {
+                player.score += 100;
+                player.y = 380;
+                player.x = 200;
+            }
+
+            break;
+        case 'down':
+            if (this.y <= 300) {
+                this.y += 80;
+            }
+
+            break;
+
+    }
+};
+function playerPositionRange(xAxis, yAxis) {
+    var coordRange = {
+        xBegin: xAxis,
+        xEnd: xAxis + 99,
+        yBegin: yAxis - 20,
+        yEnd: yAxis + 50
+    }
+    return coordRange;
+}
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var player = new Player();
+
+//Checks bugs postion to see if it matches player
+//if it does, a collision has occured
+function checkCollision(x, y) {
+    var coords = playerPositionRange(x, y);
+    let pad = 37;
+    allEnemies.forEach(ene => {
+        if ((ene.x + pad >= coords.xBegin - pad && ene.x + pad <= coords.xEnd - pad) && (ene.y >= coords.yBegin && ene.y <= coords.yEnd)) {
+            //collision has occured, resets your position back to the start and removes a life if you have one.
+            player.y = 380;
+            player.lives -= 1;
+            //if you have zero lives the game will end and a scoreboard will popup with your score.
+            if (player.lives === 0) {
+                alert(`Game Over!\nHere is your score ${player.score}\nOnce you click Ok. Your game will restart! `);
+                player.score = 0;
+                location.reload()
+
+            }
+        }
+    });
+
+    return coords.end;
+}
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
